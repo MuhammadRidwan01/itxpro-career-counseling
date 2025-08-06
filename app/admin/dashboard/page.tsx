db4d52f3108b7e7136d178763ab0457a102d476b
vanya <vanyashaumy@gmail.com>
2025-08-06 12:03:10 +0700

    logout admin

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -29,6 +29,8 @@ import { ImportExcelModal } from "@/components/admin/import-excel-modal"
 import { StudentModal } from "@/components/admin/student-modal"
 import { KonselingModal } from "@/components/admin/konseling-modal"
 import { KonselingBatchModal } from "@/components/admin/konseling-batch-modal"
+import { LogOut } from "lucide-react"
+import { signOut } from "next-auth/react"
 
 interface DashboardStats {
   totalSiswa: number
@@ -205,16 +207,32 @@ export default function AdminDashboard() {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-100">
-      <div className="container mx-auto px-4 py-6 max-w-7xl">
+      <div className="container mx-auto px-6 py-6 max-w-7xl">
         {/* Header */}
-        <div className="mb-8">
-          <div className="flex items-center gap-3 mb-2">
-            <div className="w-8 h-8 bg-gradient-to-r from-indigo-600 to-blue-600 rounded-lg flex items-center justify-center">
-              <BarChart3 className="w-5 h-5 text-white" />
+        <div className="mb-8 flex justify-between">
+          <div>
+            <div className="flex items-center gap-3 mb-2">
+              <div className="w-8 h-8 bg-gradient-to-r from-indigo-600 to-blue-600 rounded-lg flex items-center justify-center">
+                <BarChart3 className="w-5 h-5 text-white" />
+              </div>
+              <h1 className="text-2xl font-bold text-slate-800">Admin Dashboard</h1>
             </div>
-            <h1 className="text-2xl font-bold text-slate-800">Admin Dashboard</h1>
+            <p className="text-slate-600 text-sm">Kelola sistem konseling karir SMK ITXPRO</p>
           </div>
-          <p className="text-slate-600 text-sm">Kelola sistem konseling karir SMK ITXPRO</p>
+          <PremiumButton
+                className="text-red-400 hover:bg-transparent bg-transparent"
+                variant="secondary"
+                size="sm"
+                onClick={() => {
+                  const confirmLogout = window.confirm("Apakah Anda yakin ingin logout?");
+                  if (confirmLogout) {
+                    signOut({ callbackUrl: "/auth/admin" })
+                  }
+                }}
+              >
+                <LogOut className="w-4 h-" />
+                Logout
+          </PremiumButton>
         </div>
 
         {/* Stats Cards */}
